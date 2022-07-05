3b16170236ecd720972fc309f3034bdac6a4f5dd
Sxizman <germanbikbow@gmail.com>
2022-07-05 15:12:26 +0300

    Added tasks from new chapter

---
+++ GeometryPainting/SegmentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+
+namespace GeometryPainting
+{
+    public static class SegmentColorExtension
+    {
+        private static Dictionary<Segment, Color> _colors = new Dictionary<Segment, Color>();
+
+        public static void SetColor(this Segment s, Color c)
+        {
+            _colors[s] = c;
+        }
+
+        public static Color GetColor(this Segment s)
+        {
+            return _colors.ContainsKey(s) ? _colors[s] : Color.Black;
+        }
+    }
+}
